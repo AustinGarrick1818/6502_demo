6e36a33a073a02c9c6b612ff513648a2fd68018d
AustinGarrick1818 <austingarrick752@gmail.com>
2021-04-03 02:31:41 -0700

    -

--- demo_6502/6502.h
+++ demo_6502/6502.h
@@ -5,3 +5,7 @@
 
 // Allow 1MB ram for CPU usage
 #define MAX_RAM_LEN 1024
+
+// Vectors
+#define RESET_VECT 0xFFFC
+#define SP_RESET 0x0100
--- demo_6502/demo_6502.cpp
+++ demo_6502/demo_6502.cpp
@@ -4,8 +4,21 @@
 
 #include "6502.h"
 
-using byte = unsigned char; // 8 bit type
-using word = unsigned short; // 16 bit type
+using byte = unsigned char; // 8bit type
+using word = unsigned short; // 16bit type
+using uint32 = unsigned int; // 32bit type
+
+struct RAM {
+	// Area to store our CPU information
+	byte data[MAX_RAM_LEN];
+
+	void mem_init() {
+		uint32 i;
+		for (i = 0; i < MAX_RAM_LEN; i++) {
+			data[i] = 0;
+		}
+	}
+};
 
 struct CPU {
 	word PC; // Program counter
@@ -17,16 +30,60 @@ struct CPU {
 	byte Y;
 
 	// Status flags
-	byte C : 1;
-	byte Z : 1;
-	byte I : 1;
-	byte D : 1;
-	byte B : 1;
-	byte V : 1;
-	byte N : 1;
+	byte C : 1; // Carry flag
+	byte Z : 1; // Zero flag
+	byte I : 1; // Interrupt disable
+	byte D : 1; // Decimal mode flag
+	byte B : 1; // Break command
+	byte V : 1; // Overflow flag
+	byte N : 1; // Negitive flag
+
+
+	// CPU functions
+	void reset( RAM& memory ) {
+		// Reset program counter and stack pointer
+		PC = RESET_VECT;
+		SP = SP_RESET;
+
+		// Reset status flags
+		C = 0;
+		Z = 0;
+		I = 0;
+		D = 0;
+		B = 0;
+		V = 0;
+		N = 0;
+
+		// Clear registers
+		A = 0;
+		X = 0;
+		Y = 0;
+
+		// Initialize RAM
+		memory.mem_init();
+	}
+
+	void grab(uint32 cycles, RAM& memory) {
+
+	}
+
+	void exec( uint32 cycles, RAM& memory ) {
+		while (cycles > 0) {
+			byte ins = grab(cycles, memory);
+		}
+	}
 };
 
 int main()
 {
+	CPU cpu;
+	RAM ram;
+
+	// Simulate a poweron/reset
+	cpu.reset( ram );
+
+	// Execute instruction in memory
+	cpu.exec(mem);
+
 	return 0;
 }
